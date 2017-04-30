b8766f039864cd85a576374de928f32b7fe31a97
Michael Herman <hermanmu@gmail.com>
2017-04-30 12:32:58 -0600

    added base images

--- tests/main.js
+++ tests/main.js
@@ -15,7 +15,10 @@ config.forEach((el) => {
     await t.takeScreenshot(`${el.title}-${imageName}`);
     if (imageName === 'actual') {
       const results = await compareImages(el.title);
-      if (!results) throw new Error(`${el.title} images are different`);
+      if (!results) {
+        el.same = false;
+        throw new Error(`${el.title} images are different`);
+      }
     }
   });
 })
